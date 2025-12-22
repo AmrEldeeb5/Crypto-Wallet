bfe59b2b5df223ff8def3ff8b080bb087a97f1ca
amreldeeb <amreldeeb641@gmail.com>
2025-12-22 11:14:52 +0200

    add PortfolioDatabase with Room configuration and DAO access

--- composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/core/database/portfolio/PortfolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/core/database/portfolio/PortfolioDatabase.kt
@@ -1,2 +1,12 @@
 package com.example.cryptowallet.app.core.database.portfolio
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.cryptowallet.app.portfolio.data.local.PortfolioCoinEntity
+import com.example.cryptowallet.app.portfolio.data.local.PortfolioDao
+
+
+@Database(entities = [PortfolioCoinEntity::class], version = 1)
+abstract class PortfolioDatabase: RoomDatabase() {
+    abstract fun portfolioDao(): PortfolioDao
+}
