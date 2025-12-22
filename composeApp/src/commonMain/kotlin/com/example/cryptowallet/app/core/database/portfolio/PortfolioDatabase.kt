f6116c867b927918e4ecec0df3581d8748ba2b87
amreldeeb <amreldeeb641@gmail.com>
2025-12-22 17:57:48 +0200

    add UserBalanceEntity and UserBalanceDao to PortfolioDatabase; update schema version to 2

--- composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/core/database/portfolio/PortfolioDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/example/cryptowallet/app/core/database/portfolio/PortfolioDatabase.kt
@@ -4,9 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.cryptowallet.app.portfolio.data.local.PortfolioCoinEntity
 import com.example.cryptowallet.app.portfolio.data.local.PortfolioDao
+import com.example.cryptowallet.app.portfolio.data.local.UserBalanceDao
+import com.example.cryptowallet.app.portfolio.data.local.UserBalanceEntity
 
 
-@Database(entities = [PortfolioCoinEntity::class], version = 1)
+@Database(entities = [PortfolioCoinEntity::class, UserBalanceEntity::class], version = 2)
 abstract class PortfolioDatabase: RoomDatabase() {
     abstract fun portfolioDao(): PortfolioDao
+    abstract fun UserBalanceDao(): UserBalanceDao
 }
